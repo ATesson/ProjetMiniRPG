5cd77671cdd6515dcd9995deeedb02655e36b74b
flomalard <147619271+flomalard@users.noreply.github.com>
2023-10-27 17:18:27 +0200

    Add files via upload

--- Models/Personnage.js
+++ Models/Personnage.js
@@ -1,60 +1,48 @@
 class Personnage {
-    constructor(nom, sante = 200, mana = 200, niveau = 1, competence = "") {
-      this.nom = nom;
-      this.sante = sante;
-      this.mana = mana;
-      this.niveau = niveau;
-      this.competence = competence;
-    }
-  
-    attaquer() {
-      console.log(`${this.nom} effectue une attaque de base.`);
-    }
-  
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} utilise sa compétence spéciale : ${this.competence}.`);
-    }
-  
-    prendreDegats(degats) {
-      this.sante -= degats;
-      console.log(`${this.nom} perd ${degats} points de santé. Santé restante : ${this.sante}`);
-    }
-  
-    boirePotion() {
-      this.sante += 50;
-      this.mana += 50;
-      console.log(`${this.nom} boit une potion de soin et restaure 50 points de santé et de mana.`);
-    }
+  constructor(nom, sante = 200, mana = 200, niveau = 1, competence = "") {
+    this.nom = nom;
+    this.sante = sante;
+    this.mana = mana;
+    this.niveau = niveau;
+    this.competence = competence;
   }
-  
-  class Feu extends Personnage {
-    constructor(nom) {
-      super(nom, 180, 220, 1, "Explosion de Feu");
-    }
-  
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} déclenche une puissante ${this.competence} infligeant des dégâts de zone.`);
-    }
+
+  attaquer() {
+    console.log(`${this.nom} effectue une attaque de base.`);
   }
-  
-  class Eau extends Personnage {
-    constructor(nom) {
-      super(nom, 220, 180, 1, "Vague Géante");
-    }
-  
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} invoque une ${this.competence} pour submerger l'ennemi.`);
-    }
+
+
+  prendreDegats(degats) {
+    this.sante -= degats;
+    console.log(`${this.nom} perd ${degats} points de santé. Santé restante : ${this.sante}`);
   }
-  
-  class Terre extends Personnage {
-    constructor(nom) {
-      super(nom, 200, 200, 1, "Mur de Pierres");
-    }
-  
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} crée un ${this.competence} pour se protéger des attaques.`);
-    }
+
+  boirePotion() {
+    this.sante += 50;
+    this.mana += 50;
+    console.log(`${this.nom} boit une potion de soin et restaure 50 points de santé et de mana.`);
   }
+}
+
+export class Feu extends Personnage {
+  constructor(nom) {
+    super(nom, 180, 220, 1, "Explosion de Feu");
+  }
+
+}
+
+export class Eau extends Personnage {
+  constructor(nom) {
+    super(nom, 220, 180, 1, "Tsunami");
+  }
+
+}
+
+export class Terre extends Personnage {
+  constructor(nom) {
+    super(nom, 150, 150, 1, "Lancé de Pierres");
+  }
+
+}
 
 
