41cc2bd82a5b7a1f120fc004356d71f7bf332a7f
Andreea <andreea@stern.consulting>
2023-10-27 12:22:28 +0200

    choix des personnages

--- Models/Personnage.js
+++ Models/Personnage.js
@@ -11,9 +11,6 @@ class Personnage {
       console.log(`${this.nom} effectue une attaque de base.`);
     }
   
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} utilise sa compétence spéciale : ${this.competence}.`);
-    }
   
     prendreDegats(degats) {
       this.sante -= degats;
@@ -27,34 +24,25 @@ class Personnage {
     }
   }
   
-  class Feu extends Personnage {
+  export class Feu extends Personnage {
     constructor(nom) {
       super(nom, 180, 220, 1, "Explosion de Feu");
     }
   
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} déclenche une puissante ${this.competence} infligeant des dégâts de zone.`);
-    }
   }
   
-  class Eau extends Personnage {
+  export class Eau extends Personnage {
     constructor(nom) {
       super(nom, 220, 180, 1, "Vague Géante");
     }
   
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} invoque une ${this.competence} pour submerger l'ennemi.`);
-    }
   }
   
-  class Terre extends Personnage {
+  export class Terre extends Personnage {
     constructor(nom) {
       super(nom, 200, 200, 1, "Mur de Pierres");
     }
   
-    utiliserCompetenceSpeciale() {
-      console.log(`${this.nom} crée un ${this.competence} pour se protéger des attaques.`);
-    }
   }
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,37 @@
-function startCombat() {
-    const selectedCategory = document.querySelector('input[name="fighterCategory"]:checked');
-    const selectedDifficulty = document.getElementById('difficulty');
+// const {Feu, Terre, Eau} = require('../../Models/Personnage.js')
+// import { Feu, Eau, Terre } from "../../Models/Personnage.js";
 
-    if (selectedCategory && selectedDifficulty) {
-        // Rediriger vers la page d'accueil (index.html) en fonction des sélections
-        window.location.href = `/ProjetMiniRPG/index.html`;
-    } else {
-        alert("Veuillez sélectionner un combattant et un niveau de difficulté.");
-    }
+function startCombat() {
+  const selectedCategory = document.querySelector(
+    'input[name="fighterCategory"]:checked'
+  );
+  const selectedDifficulty = document.getElementById("difficulty");
+
+  if (selectedCategory && selectedDifficulty) {
+    const character = selectedCategory.value;
+    const difficulty = selectedDifficulty.value;
+    window.location.href = `/ProjetMiniRPG/combat.html?character=${character}&difficulty=${difficulty}`;
+  } else {
+    alert("Veuillez sélectionner un combattant et un niveau de difficulté.");
+  }
 }
+
+// const selectedCategory = document.querySelectorAll(
+//   'input[name="fighterCategory"]'
+// );
+
+// var fighter;
+
+// const fighterChoice = () => {
+//   selectedCategory.forEach((category) => {
+//     category.addEventListener("click", (event) => {
+        
+//       if (event.target.value == "feu") {
+//         fighter = new Feu("Fire-Mage");
+//       }
+//       console.log(fighter);
+//       return fighter;
+//     });
+//   });
+// };
+// fighterChoice();
